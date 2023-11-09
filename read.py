c83f72a156c2a2c3655926f1327476498b39113e
Abdul malik <azizyamin8@gmail.com>
2023-11-09 13:33:16 +0530

    first commit

---
+++ read.py
@@ -0,0 +1,21 @@
+# installed library mysql-connector-python
+import mysql.connector
+
+
+#creating connection
+class read:
+    def __init__(self):
+        self.connection=mysql.connector.connect(
+            user="root",
+            password='Abdulmalik8@',
+            database="bank_manegment"
+        )
+
+    def specific_info(self,ID,table_name,column_name):
+        cur=self.connection.cursor()
+        cur.execute(f"select * from {table_name} where {column_name}={ID}")
+        print(cur.fetchall())
+        
+
+    
+             
